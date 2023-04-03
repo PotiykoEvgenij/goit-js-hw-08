9439c3808ee31f58a1aee027ee24a5eb01bbe5d7
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-04-03 22:23:37 +0300

    hw8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(function(event) {
+  localStorage.setItem(LOCAL_STORAGE_KEY, event.seconds);
+}, 1000));
+
+const currentTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
 
